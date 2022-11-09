7ed5a2cfcea63d75f25630ce0fb10c8d5daa2fb4
BenBill <ben.billings.02@icloud.com>
2022-11-08 21:09:06 -0500

    working on routes.....also added bootstrao

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -40,13 +40,68 @@ router.get('/:id', async (req, res) => {
             return;
         }
 
-        res.status(200).json(userInfo)
+        res.status(200).json(userInfo);
 
     } catch(err) {
         res.status(500).json(err);
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const userInfo = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+        
+        req.session.save(() => {
+            req.session.user_id = userInfo.id;
+            req.session.username = userInfo.username;
+            req.session.logged_in = true;
 
+            res.status(200).json(userInfo);
+
+        })
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = await User.findOne({ where: { email: req.body.email } });
+        if (!userInfo) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again.'});
+            return;
+        }
+        
+        const validPassword = await userInfo.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again.'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userInfo, message: 'Login successful!' });
+        });
+    
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
